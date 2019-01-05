47be3d155ce4f9310964f5e383ef3a4553dbfa34
fwxks <mrpcmn@protonmail.com>
2019-01-05 17:39:56 -0300

    Added Openssl EVP digest routines

--- src/hasher.cpp
+++ src/hasher.cpp
@@ -3,57 +3,76 @@
 namespace Hasher {
     /* Code based on https://stackoverflow.com/questions/17156282/passing-a-stdarray-of-unknown-size-to-a-function (Ans by Andy Prowl)*/
     template<std::size_t SIZE>
-    std::string hash_hex(const std::array<unsigned char, SIZE> &hash)
+    std::string hash_hex(const std::array<unsigned char, SIZE> &hash, const uint bytes_written)
     {
+        // Create a stringstream to store the string version of the provided hash.
         std::stringstream ss;
-        for (unsigned int inx = 0; inx < SIZE; ++inx)
+        // Create a readable hash string, iterating through each byte of the array.
+        for (unsigned int inx = 0; inx < bytes_written; ++inx)
         {
-            ss << std::hex << std::setw(2) << std::setfill('0') << (uint)hash[inx];
+            ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<uint>(hash[inx]);
         }
         return ss.str();
     }
 
-    /* Code based on https://stackoverflow.com/questions/2262386/generate-sha256-with-openssl-and-c (Edited by: Jonathan Drapeau) */
-    std::string calculate_sha256(const std::unique_ptr<std::string> &msg)
+    /**
+      Get the message hash:
+      1 - Create an unsigned array of 32 (SHA 256), 20 (SHA1) or MD5(16) bytes
+      2 - Initiate a [SHA256, SHA, MD5]_CTX structure to save the updated hash on it.
+      3 - Calculate the hash.
+      4 - Store the calculated hash on the u_char array.
+
+      @param msg: A unique ptr tp the message to hash.
+      @param digest_name: Name of the hash function.
+    **/
+    std::string calculate_hash(const std::unique_ptr<std::string> &msg, const std::string &digest_name)
     {
-        std::array<unsigned char, SHA256_DIGEST_LENGTH> hash;
-        SHA256_CTX sha256;
-        SHA256_Init(&sha256);
-        SHA256_Update(&sha256, msg->c_str(), msg->length());
-        SHA256_Final(hash.data(), &sha256);
-        return hash_hex<SHA256_DIGEST_LENGTH>(hash);
-    }
+        // Use the Openssl API to convert the provided hash function name to an EVP_MD struct
+        auto md = std::make_unique<const EVP_MD *>(EVP_get_digestbyname(digest_name.c_str()));
 
-    std::string calculate_sha1(const std::unique_ptr<std::string> & msg)
-    {
-        std::array<unsigned char, SHA_DIGEST_LENGTH> hash;
-        SHA_CTX sha;
+        // Check if the EVP_MD is valid.
+        if(*md == nullptr){
+            throw Hasher::Exception(digest_name + " doesn't exists.", EXIT_FAILURE);
+        }
 
-        SHA1_Init(&sha);
-        SHA1_Update(&sha, msg->c_str(), SHA_DIGEST_LENGTH);
-        SHA1_Final(hash.data(), &sha);
+        // Init an array of 'HF_BLOCK_SIZE' bytes.
+        std::array<unsigned char, EVP_MAX_MD_SIZE> hash;
+        // Create an EVP_MD_CTX context, to store the hash on it.
+        auto mdctx = std::make_unique<EVP_MD_CTX *>(EVP_MD_CTX_new());
 
-        return hash_hex<SHA_DIGEST_LENGTH>(hash);
-    }
+        EVP_DigestInit_ex(*mdctx, *md, nullptr);
+        EVP_DigestUpdate(*mdctx, msg->c_str(), msg->length());
 
-    std::string calculate_md5(const std::unique_ptr<std::string> &msg){
-        std::array<unsigned char, MD5_DIGEST_LENGTH> hash;
-        MD5_CTX md5;
-        MD5_Init(&md5);
-        MD5_Update(&md5, msg->c_str(), msg->length());
-        MD5_Final(hash.data(), &md5);
-        return hash_hex<MD5_DIGEST_LENGTH>(hash);
+        unsigned int bytes_written;
+
+        EVP_DigestFinal_ex(*mdctx, hash.data(), &bytes_written);
+        EVP_MD_CTX_free(*mdctx);
+
+        return hash_hex<hash.size()>(hash, bytes_written);
     }
 
     File::File(const std::string &f) : pathname(f)
     {
+        // Check if file exists.
+        if(!File::exists())
+        {
+            int errsv = errno;
+            throw Hasher::Exception( pathname + " " + std::strerror(errsv), errsv);
+        }
+
+        // Open the specified file with the binary flag.
         this->ifile_stream.open(pathname, std::ios::in | std::ios::binary);
+
+        // Check for errors while opening the file.
         if(!this->ifile_stream.is_open())
-            throw std::runtime_error("Can't open " + pathname);
+        {
+            throw Hasher::Exception(std::string("Can't open ") + filename, EXIT_FAILURE);
+        }
     }
 
     File::~File()
     {
+        // Close the existing stream.
         if(ifile_stream.is_open())
             ifile_stream.close();
     }
--- src/include/hasher.hpp
+++ src/include/hasher.hpp
@@ -1,28 +1,45 @@
 #ifndef HASHER_HPP
 #define HASHER_HPP
 #include <array>
+#include <cstring>
+#include <cerrno>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <iterator>
 #include <memory>
-#include <openssl/sha.h>
+#include <openssl/evp.h>
 #include <openssl/md5.h>
+#include <openssl/sha.h>
 #include <sstream>
 #include <string>
-#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <vector>
 
 
 namespace Hasher {
-    std::string calculate_sha256(const std::unique_ptr<std::string> &);
-    std::string calculate_sha1(const std::unique_ptr<std::string> &);
-    std::string calculate_md5(const std::unique_ptr<std::string> &);
 
     template<std::size_t SIZE>
-    std::string hash_hex(const std::vector<unsigned char> &);
+    std::string hash_hex(const std::array<unsigned char, SIZE> &, const uint);
+
+    std::string calculate_hash(const std::unique_ptr<std::string> &, const std::string &);
+
+    class Exception : public std::exception{
+        private:
+            const std::string msg;
+            const int error_code;
+
+        public:
+            Exception(const std::string &m, const int err) : msg(m), error_code(err) {}
+            const char* what() const noexcept{
+                return msg.c_str();
+            }
+
+            const int code() const noexcept{
+                return error_code;
+            }
+    };
 
     class File {
         private:
@@ -32,15 +49,40 @@ namespace Hasher {
         public:
             File(const std::string &);
 
+             /**
+             * Use the *nix stat function to check if the file exists.
+             */
+            inline bool exists(){
+                struct stat st_buff;
+                std::memset(&st_buff, 0, sizeof (struct stat));
+                return (stat(pathname.c_str(), &st_buff) == 0);
+            }
+
+            inline off_t size(){
+                struct stat f_stat;
+                std::memset(&f_stat, 0, sizeof (struct stat));
+
+                if(stat(pathname.c_str(), &f_stat) == -1){
+                    int errsv = errno;
+                    throw Hasher::Exception(std::strerror(errsv), errsv);
+                }
+
+                return f_stat.st_size;
+            }
+
             /* Code taken from https://insanecoding.blogspot.com/2011/11/how-to-read-in-file-in-c.html */
             inline std::unique_ptr<std::string> read()
             {
+                // Return a unique pointer to a string filled with the file data.
                 return std::make_unique<std::string>(std::string(std::istreambuf_iterator<char>(ifile_stream), {}));
             }
 
+            // Wanted to know how to make a lambda function...
             std::string filename = [this]()
             {
+                // Get the last pathname separator ('/' on *nix).
                 size_t inx = pathname.find_last_of('/');
+                // Return the filename (not the pathname) if the index of the pathname isn't -1, if it does return pathname.
                 return (inx != pathname.npos) ? pathname.substr(inx + 1) : pathname;
             }();
 
