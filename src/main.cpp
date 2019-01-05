6d29fbbf7807642d39dc7c4421800c24c2fe10b3
fwxks <mrpcmn@protonmail.com>
2019-01-05 17:43:45 -0300

    Request data from virustotal

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,44 @@
 
 void usage [[noreturn]] (const std::string &prog_name)
 {
-    std::cout << "Usage: " << prog_name << " <file>";
+    std::cout << "Usage: " << prog_name << " <file> <api-key>";
     std::cout << std::endl;
     exit(EXIT_FAILURE);
 }
 
 int main(int argc, char** argv)
 {
-    return 0;
+    int exit_status = EXIT_SUCCESS;
+
+    if(argc != 3)
+        usage(argv[0]);
+
+    try
+    {
+        Hasher::File f(argv[1]);
+        std::cout << "[*] Hashing " << f.filename << ": " << f.size() << " bytes" << '\n';
+        std::string url = Requester::set_url(std::string(argv[2]), Hasher::calculate_hash(f.read(), "sha256"));
+        std::cout << "[*] Sending data." << '\n';
+
+        Requester::get_data(url);
+        auto write_buffer = Requester::get_write_buffer();
+        Parser::load(write_buffer);
+        Parser::print_results();
+
+    }
+    catch (Hasher::Exception &r_error)
+    {
+        std::cerr << "Error (" << r_error.code() << "): " << r_error.what() << std::endl;
+        exit_status = r_error.code();
+    }
+    catch(Parser::JsonException &json_error){
+        std::cerr << "Error: " << json_error.what() << std::endl;
+        exit_status = EXIT_FAILURE;
+    }
+    catch(std::exception &ex){
+        std::cerr << "Error: " << ex.what() << std::endl;
+        exit_status = EXIT_FAILURE;
+    }
+
+    return exit_status;
 }
